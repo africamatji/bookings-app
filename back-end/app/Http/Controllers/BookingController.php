b7358da2669a34e360d0b9d85c6b506fe5e91999
africam <africamatji@gmail.com>
2023-04-05 18:17:34 +0200

    bookings controller

--- back-end/app/Http/Controllers/BookingController.php
+++ back-end/app/Http/Controllers/BookingController.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
-class BookingController
-{
+use App\Repositories\BookingRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Request;
 
+class BookingController extends Controller
+{
+    protected BookingRepository $bookingRepository;
+
+    public function __construct(BookingRepository $bookingRepository)
+    {
+        $this->bookingRepository = $bookingRepository;
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+        $booking = $this->bookingRepository->create($request->all());
+
+        return response()->json([
+            'message' => 'successful',
+            'booking' => $booking
+        ]);
+    }
 }

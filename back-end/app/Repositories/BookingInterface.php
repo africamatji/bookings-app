96e861e15646f805f626b98d352a618475d4b697
africam <africamatji@gmail.com>
2023-04-05 18:16:26 +0200

    bookings model

---
+++ back-end/app/Repositories/BookingInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Booking;
+use Illuminate\Database\Eloquent\Collection;
+
+interface BookingInterface
+{
+    public function create(array $data): Booking;
+    public function list(): Collection;
+}

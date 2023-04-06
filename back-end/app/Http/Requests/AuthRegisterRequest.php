1c46e575a6e5330534f17723b91dc6025b0ab7d1
africam <africamatji@gmail.com>
2023-04-06 15:34:12 +0200

    add failed validation response

--- back-end/app/Http/Requests/AuthRegisterRequest.php
+++ back-end/app/Http/Requests/AuthRegisterRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AuthRegisterRequest extends FormRequest
 {
@@ -26,5 +28,12 @@ class AuthRegisterRequest extends FormRequest
             'email' => 'email|required|unique:users',
             'password' => 'required'
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
--- back-end/app/Http/Requests/BookingRequest.php
+++ back-end/app/Http/Requests/BookingRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class BookingRequest extends FormRequest
 {
@@ -25,5 +27,12 @@ class BookingRequest extends FormRequest
             'reason' => 'required|string|max:255',
             'date' => 'required|string',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }

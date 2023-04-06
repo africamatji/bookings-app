33ff16eb3af83e72fe98a3817257a7bcf1c0879f
africam <africamatji@gmail.com>
2023-04-06 11:01:16 +0200

    (test) can login

--- back-end/tests/Feature/AuthTest.php
+++ back-end/tests/Feature/AuthTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -29,6 +31,32 @@ class AuthTest extends TestCase
                 'name' => $requestData['name'],
                 'email' => $requestData['email'],
             ],
+        ]);
+    }
+
+    public function test_can_login_with_valid_credentials(): void
+    {
+        $password = $this->faker->password();
+        $user = User::factory([
+            'password' => Hash::make($password)
+        ])->create();
+        $requestData = [
+            'email' => $user->email,
+            'password' => $password,
+        ];
+
+        $response = $this->postJson('/api/login', $requestData);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'user' => [
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+            ],
+            'access_token',
         ]);
     }
 }
